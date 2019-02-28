2c5ca968aafeefa91f7c8598131b73f310229be6
Muthanna A. Alwahash <mkhuthir@gmail.com>
2019-02-28 23:21:08 +0400

    progs: added more examples

---
+++ sense_hat/clear.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+from sense_hat import SenseHat
+
+s = SenseHat()
+s.clear()
+
---
+++ sense_hat/set_pixles2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+# Define some colours
+g = (0, 255, 0) # Green
+b = (0, 0, 0) # Black
+
+# Set up where each colour will display
+creeper_pixels = [
+    g, g, g, g, g, g, g, g,
+    g, g, g, g, g, g, g, g,
+    g, b, b, g, g, b, b, g,
+    g, b, b, g, g, b, b, g,
+    g, g, g, b, b, g, g, g,
+    g, g, b, b, b, b, g, g,
+    g, g, b, b, b, b, g, g,
+    g, g, b, g, g, b, g, g
+]
+
+# Display these colours on the LED matrix
+sense.set_pixels(creeper_pixels)
